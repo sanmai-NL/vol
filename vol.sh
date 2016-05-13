22222db8f44f04552f581dcb2907dd27b96a1f57
Sander Maijers <S.N.Maijers@gmail.com>
2016-05-13 13:40:48 +0200

    allow extra parameter passing to CLI utils
    * enhance: Allow the user to pass parameters such as -v per function.
    * docs: Give more examples.

--- vol.sh
+++ vol.sh
@@ -3,14 +3,16 @@
 ## A minimal POSIX shell library to help manage Docker data volumes.
 
 dir_out() {
-    ## Pipes out a tar -f target as pax stream.
+    ## Pipes out a directory as pax stream.
 
     ## $1: A root directory to input.
     TARGET_IN="$1"
-    ## $2: A tar -f target to output. Default: pipe.
-    TARGET_OUT="${2:--}"
+    ## $2: Extra parameters to pass on to tar.
+    PARAMS="$2"
+    ## $3: A tar -f target to output. Default: pipe.
+    TARGET_OUT="${3:--}"
 
-    tar -cvp --format='posix' --numeric-owner -f "$TARGET_OUT" -C "$TARGET_IN" '.'
+    tar $PARAMS -cp --numeric-owner -f "$TARGET_OUT" -C "$TARGET_IN" '.'
 }
 
 vol_out() {
@@ -18,39 +20,45 @@ vol_out() {
 
     ## $1: A volume.
     VOL="$1"
+    ## $2: Extra parameters to pass on to tar.
+    PARAMS="$2"
 
-    sudo docker run --attach='stderr' --attach='stdout' --rm --volume="$VOL"':/mnt/:ro' 'alpine' \
-        tar -cpv -f '-' -C '/mnt/' '.'
+    sudo -- docker run --interactive --volume="${VOL}:/mnt/:ro" 'alpine' \
+        tar $PARAMS -cp -f '-' -C '/mnt/' '.'
 }
 
 vol_in() {
     ## Extracts tar stream into a volume.
 
-    ## $1: volume
+    ## $1: A volume.
     VOL="$1"
+    ## $2: Extra parameters to pass on to tar.
+    PARAMS="$2"
 
-    sudo docker run --attach='stdin' --interactive --rm --volume="$VOL"':/mnt/:rw' 'alpine' \
-        tar -xpv -f '-' -C '/mnt/'
+    sudo -- docker run --interactive --rm --volume="${VOL}:/mnt/:rw" 'alpine' \
+        tar $PARAMS -xp -f '-' -C '/mnt/'
 }
 
 vol_clear() {
     ## Clears a volume, removing all its files and directories.
 
-    ## $1: volume
+    ## $1: A volume.
     VOL="$1"
+    ## $2: Extra parameters to pass on to rm.
+    PARAMS="$2"
 
-    sudo docker run --rm --volume="$VOL"':/mnt/:rw' 'alpine' \
-        find '/mnt/' -mindepth '1' -exec rm -frv '{}' +
+    sudo -- docker run --rm --volume="${VOL}:/mnt/:rw" 'alpine' \
+        rm $PARAMS -fr '/mnt/'
 }
 
 vol_ls() {
     ## Lists files and directories in the root of a volume.
 
-    ## $1: volume
+    ## $1: A volume.
     VOL="$1"
+    ## $1: Extra parameters to pass on to ls.
+    PARAMS="$2"
 
-    sudo docker run --rm --volume="$VOL"':/mnt/:ro' 'alpine' \
-        ls -ahl '/mnt/'
+    sudo -- docker run --rm --volume="$VOL"':/mnt/:ro' 'alpine' \
+        ls $PARAMS -ahl '/mnt/'
 }
-
-## TODO: Safe rsync functionality.
